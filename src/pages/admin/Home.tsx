0c38b156b44e9a9f3c8758352f520d803a5187c9
--CarlosKD12-- <carlos.mendoza32@unmsm.edu.pe>
2024-06-03 02:04:44 -0500

    terminado esqueleto del menu

--- src/pages/admin/Home.tsx
+++ src/pages/admin/Home.tsx
@@ -1,4 +1,7 @@
 import adminfoto from "../../assets/adminfoto.png";
+import registerIcon from "../../assets/registro.png";
+import userIcon from "../../assets/usuario.png";
+
 import "../../css/home.css"
 export const Home = () => {
   return (
@@ -9,11 +12,28 @@ export const Home = () => {
               <div className="btnvolver"> <button> volver </button> </div>
             </div>
 
-            <div className="menu">
+            <div className="menutitle">
               <h1>Menu</h1>
             </div>
 
-            <div></div>
+                        <div className="container">
+              <div className="registerSection">
+                <div className="registerBTN">
+                  <button className="image1Button">
+                    <img src={registerIcon} alt="registro" className="buttonRegistro"/>
+                  </button>
+                </div>
+                <div className="registerTXT"> Registros </div>
+              </div>
+              <div className="userSection">
+                <div className="userBTN">
+                  <button className="image2Button">
+                    <img src={userIcon} alt="user" className="buttonUsuario"/>
+                  </button>
+                </div>
+                <div className="userTXT"> Usuarios </div>
+              </div>
+            </div>
 
         </div>
     </>
