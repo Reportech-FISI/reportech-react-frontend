ddc50654cbb3475a3ce5d13f95e872879f88d3aa
Braulio Saldaña <136661206+BrauSal2103@users.noreply.github.com>
2024-06-08 01:25:22 -0500

    Detalles del registro
    Haganlo correr ustedes xfa pq cada q intento correr el codigo me acaba mandando siempre al "Home admin"
    (No se usar React y me mandan a hacer uno :c )

--- src/pages/registers/RegisterDetails.tsx
+++ src/pages/registers/RegisterDetails.tsx
@@ -1,7 +1,62 @@
+import './RegisterDetails.css';
 export const RegisterDetails = () => {
   return (
-    <div>
-      <h1 className="text-5xl">Register Details page</h1>
+    <div className="container">
+      <div className="header">
+        <div className="profile">
+          <img src="profile-placeholder.png" alt="Profile Picture" />
+          <div>
+            <p>Nombre1 Apellido1</p>
+            <p>(Tipo de cuenta)</p>
+          </div>
+        </div>
+        <button> ← Volver</button>
+      </div>
+      <h1>Detalles</h1>
+      <div>
+        <div className="section-title">Datos del registro:</div>
+        <table>
+          <tbody>
+            <tr>
+              <td>ID:</td>
+              <td>Título:</td>
+            </tr>s
+            <tr>
+              <td>Fecha:</td>
+              <td>Estado:</td>
+            </tr>
+            <tr>
+              <td>Prioridad:</td>
+              <td>Ubicación:</td>
+            </tr>
+            <tr>
+              <td colSpan={2}>Usuario reportador:</td>
+            </tr>
+            <tr>
+              <td colSpan={2}>Encargado asignado:</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div>
+        <div className="section-title">Datos del equipo:</div>
+        <table>
+          <tbody>
+            <tr>
+              <td>ID:</td>
+              <td>Nombre:</td>
+              <td rowSpan={3} className="photo">Foto:</td>
+            </tr>
+            <tr>
+              <td colSpan={2}>Descripción del problema:</td>
+            </tr>
+            <tr>
+              <td colSpan={2}>Estado de reparación:</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
+
