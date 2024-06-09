6eacebd73e3ced12cd95923a9a1ac75e79e61ea1
Braulio Saldaña <136661206+BrauSal2103@users.noreply.github.com>
2024-06-09 01:31:33 -0500

    Detalles del registro (09/06)
    A la espera de revisión.

--- src/pages/registers/RegisterDetails.tsx
+++ src/pages/registers/RegisterDetails.tsx
@@ -3,7 +3,7 @@ export const RegisterDetails = () => {
   return (
     <>
     <Appbar/>
-    <div className="max-w-4xl mx-auto my-5 p-5 bg-white rounded-lg shadow-md">
+    <div className="container mx-auto my-5 p-5 bg-white rounded-lg shadow-md">
       <h1 className="text-3xl font-bold mb-5">Detalles</h1>
       <div>
         <div className="text-lg font-semibold mb-2">Datos del registro:</div>
@@ -32,15 +32,15 @@ export const RegisterDetails = () => {
       </div>
       <div>
         <div className="text-lg font-semibold mt-5 mb-2">Datos del equipo:</div>
-        <table className="w-full border-collapse">
+        <table className="w-full border-collapse table-auto">
           <tbody>
             <tr>
               <td className="border p-2">ID:</td>
-              <td className="border p-2" >Nombre:</td>
+              <td className="border p-2 w-2/4 whitespace-normal break-words">Nombre:</td>
               <td className="border p-2 align-text-top" rowSpan={3}>Foto:</td>
             </tr>
             <tr>
-              <td className="border p-2 text-balance" colSpan={2}>Descripción del problema:</td>
+              <td className="border p-2 w-3/4 whitespace-normal break-words" colSpan={2}>Descripción del problema: </td>
             </tr>
             <tr>
               <td className="border p-2" colSpan={2}>Estado de reparación:</td>
